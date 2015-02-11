354cab26d0fa6fddaf6baca8adb4d3da52fa2566
mteper <michael.teper@elloco.com>
2015-02-10 16:59:11 -0800

    NuGet and dependency updates  - added .nuspec file  - fixed HintPath  - updated to latest dependency packages

--- SimpleWorkflowFramework.NET/Properties/AssemblyInfo.cs
+++ SimpleWorkflowFramework.NET/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SimpleWorkflowFramework.NET")]
-[assembly: AssemblyCopyright("Copyright ©  2013 Shawn Debnath")]
+[assembly: AssemblyCopyright("Copyright ©  2013-2015 Shawn Debnath")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0.0")]
